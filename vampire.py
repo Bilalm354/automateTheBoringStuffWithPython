1bc061fbe9ffb7cbf0f8c2fdfc94712482577fbd
Bilal <bilalm354@gmail.com>
2019-11-04 01:40:00 +0000

    .

---
+++ vampire.py
@@ -0,0 +1,10 @@
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print ('unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alive, grannie.')
+else:
+    print('Who the fook is that guy?')
